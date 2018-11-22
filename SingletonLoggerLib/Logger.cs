cf2094dd359954d41c39d974650d1ae4944e5b87
Tarak-Chandra-Sarkar <tarak.chandra.sarkar@hotmail.com>
2018-11-22 21:43:32 +0530

    Initial commit

---
+++ SingletonLoggerLib/Logger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SingletonLoggerLib
+{
+    public enum LogLevel
+    {
+        INFO = 1,
+        WARNING = 2,
+        ERROR = 3
+    }
+
+    public class Logger
+    {
+        private static Logger _logger = null;
+        private static readonly object _classLock = typeof(Logger);
+        private static readonly object _locker = new object();
+
+        private static StreamWriter _output = null;
+        public static string _logFilePath = @"LogDetails.log";
+
+
+        private Logger()
+        {
+        }
+
+        public static Logger GetLoggerInstance()
+        {
+            lock (_classLock)
+            {
+                if (_logger == null)
+                {
+                    _logger = new Logger();
+                }
+            }
+
+            return _logger;
+        }
+
+        public void LogMessage(string msg, LogLevel logLevel)
+        {
+            try
+            {
+                lock (_locker)
+                {
+                    if (_output == null)
+                    {
+                        _output = new StreamWriter(_logFilePath, true, UnicodeEncoding.Default);
+                    }
+
+                    _output.WriteLine(DateTime.Now + " | " + logLevel.ToString().PadRight(7, ' ') +" | " + msg, new object[0]);
+
+                    if (_output != null)
+                    {
+                        _output.Close();
+                        _output = null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, new object[0]);
+                throw;
+            }
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using SingletonLoggerLib;
+
+namespace SingletonPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Logger logger = Logger.GetLoggerInstance();
+            Logger logger2 = Logger.GetLoggerInstance();
+            Logger logger3 = Logger.GetLoggerInstance();
+
+            Task[] tasks = new Task[]
+            {
+            Task.Factory.StartNew(()=>
+                                logger.LogMessage("This is Singleton Pattern implementation in Logging helper library", LogLevel.INFO) ),
+
+            Task.Factory.StartNew(() =>
+                                logger2.LogMessage("This is Singleton Pattern implementation in Logging helper library", LogLevel.WARNING)),
+
+            Task.Factory.StartNew(() =>
+                                logger3.LogMessage("This is Singleton Pattern implementation in Logging helper library", LogLevel.ERROR))
+            };
+
+            //Block until all tasks complete.
+            Task.WaitAll(tasks);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
